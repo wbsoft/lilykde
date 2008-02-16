ba3e537b6c09909ed63a4d5769c15d086fd72885
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-02-16 21:36:02 +0000

    clean and clear up a bit
    git-svn-id: svn://localhost/tmp/repo1/trunk@66 e61d349e-2442-45d7-aecb-3eed713e9aed

--- lilykde_i18n.py
+++ lilykde_i18n.py
@@ -3,7 +3,9 @@ from locale import getdefaultlocale
 
 # TODO: in system-wide installation use standard locale dirs and lilykde
 # textdomain
-localedir = os.path.join (os.path.dirname(__file__), "..", "mo")
+
+# Find sibling dir mo/ in parent of current script dir
+modir = os.path.join(os.path.dirname(__file__), "..", "mo")
 
 _ = lambda s: s
 
@@ -12,7 +14,7 @@ try:
     if lang:
         for mofile in lang, lang.split("_")[0]:
             try:
-                fp = open(os.path.join(localedir, mofile + ".mo"))
+                fp = open(os.path.join(modir, mofile + ".mo"))
                 import gettext
                 _ = gettext.GNUTranslations(fp).ugettext
                 break
