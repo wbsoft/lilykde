e8976d2e98975d69b727cdb4e636ed9a2d21efd7
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-03-01 07:42:39 +0000

    update hyphen.py
    git-svn-id: svn://localhost/tmp/repo1/trunk@144 e61d349e-2442-45d7-aecb-3eed713e9aed

--- hyphen.py
+++ hyphen.py
@@ -1,15 +1,18 @@
-
 """
 
-'a3c5b' --> 'acb': '035'
+This is a Python module to hyphenate text.
 
-beter:
-    'acb': (0,3,5)
-    
-    
-    
-    
-    
+It is inspired by Ruby's Text::Hyphen, but currently reads standard *.dic files,
+that must be installed separately.
+
+In the future it's maybe nice if dictionaries could be distributed together with
+this module, in a slightly prepared form, like in Ruby's Text::Hyphen.
+
+Wilbert Berendsen, March 2008
+info@wilbertberendsen.nl
+
+License: LGPL.
+
 """
 
 import sys
@@ -17,37 +20,41 @@ import re
 
 
 class Hyphenator(object):
-    
+
     _comments_re    = re.compile(r'%.*$')
     _hex_re         = re.compile(r'\^{2}([0-9a-f]{2})')
     _zero_start_re  = re.compile(r'^(?=\D)')
     _zero_insert_re = re.compile(r'(\D)(?=\D)')
-    
+
     def _hexrepl(self, matchObj):
         return unichr(int(matchObj.group(1), 16))
-    
-    def __init__(self):
+
+    def __init__(self, files, left=2, right=2):
+        self.left = left
+        self.right = right
         self.patterns = {}
-        self.left = 1
-        self.right = 1
-    
-    def readfile(self, filename):
-        
+        self.cache = {}
+        if type(files) not in (tuple, list):
+            files = (files,)
+        for f in files: self._readfile(f)
+
+    def _readfile(self, filename):
+
         f = open(filename)
-        
+
         charset = f.readline().strip()
         if charset.startswith('charset '):
             charset = charset[7:].strip()
-        
+
         for line in f:
             line = line.decode(charset)
-            
+
             # remove comments and whitespace
             line = self._comments_re.sub('', line.strip())
-        
+
             # replace ^^hh with the real character
             line = self._hex_re.sub(self._hexrepl, line)
-        
+
             for pat in re.split(r'\s+', line):
                 pat = self._zero_insert_re.sub(r'\g<1>0', pat)
                 pat = self._zero_start_re.sub('0', pat)
@@ -55,25 +62,27 @@ class Hyphenator(object):
                 value = tuple(map(int, pat[0::2]))
                 self.patterns[tag] = value
         f.close()
-        
+
     def hyphenate(self, word):
         word = word.lower()
-        # todo: implement caching
-        
-        word = '.%s.' % word
-        result = [0] * (len(word) + 1)
-        
-        for i in range(len(word)):
-            for j in range(i, len(word)):
-                s = word[i:j+1]
-                if s in self.patterns:
-                    v = self.patterns[s]
-                    result[i:i+len(v)] = map(max, zip(v, result[i:i+len(v)]))
-        
-        points = [i - 1 for i,r in enumerate(result) if r % 2]
+        if word in self.cache:
+            points = self.cache[word]
+        else:
+            prepWord = '.%s.' % word
+            result = [0] * (len(prepWord) + 1)
+            for i in range(len(prepWord)):
+                for j in range(i, len(prepWord)):
+                    s = prepWord[i:j+1]
+                    if s in self.patterns:
+                        v = self.patterns[s]
+                        result[i:i+len(v)] = map(max, zip(v, result[i:i+len(v)]))
+
+            points = [i - 1 for i,r in enumerate(result) if r % 2]
+            self.cache[word] = points
+
         # correct for left and right
-        right = len(word) - self.right - 2
-        return [i for i in points if i >= self.left and i <= right]
+        right = len(word) - self.right
+        return [i for i in points if self.left <= i <= right]
 
     def visualise(self, word, hyphen='-'):
         l = list(word)
@@ -81,8 +90,7 @@ class Hyphenator(object):
             l[p:p] = hyphen
         return u''.join(l)
 
-p = Hyphenator()
-p.readfile("hyph_nl.dic")
+p = Hyphenator("hyph_nl.dic", left=1, right=1)
 #print repr(p.patterns)
 
 print repr(p.visualise(sys.argv[1]))
