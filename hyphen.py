b49509f73e4fa3841c9978cf508ebd40bc988bf2
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-03-07 06:59:34 +0000

    support nonstandard hyphen patterns. there is still a bug with the index tracking
    git-svn-id: svn://localhost/tmp/repo1/trunk@158 e61d349e-2442-45d7-aecb-3eed713e9aed

--- hyphen.py
+++ hyphen.py
@@ -20,27 +20,55 @@ import re
 
 
 # cache of hyph_*.dic file per-file patterns
-_hdcache = {}
+hdcache = {}
 
 # precompile some stuff
-_re_hex = re.compile(r'\^{2}([0-9a-f]{2})').sub
-_re_parse = re.compile(r'(\d?)(\D?)').findall
+parse_hex = re.compile(r'\^{2}([0-9a-f]{2})').sub
+parse = re.compile(r'(\d?)(\D?)').findall
 
-def _hexrepl(matchObj):
+def hexrepl(matchObj):
     return unichr(int(matchObj.group(1), 16))
 
 
-def int_alt(alt):
+class parse_alt(object):
     """
-    return a factory that instantiates ints with alt attached as
-    the .alt attribute.
+    Parse nonstandard hyphen pattern alternative.
+    The instance returns a special int with data about the current position
+    in the pattern when called with an odd value.
     """
-    class Point(int):
-        def __new__(cls, value):
-            obj = int.__new__(cls, value)
-            obj.alt = alt
-            return obj
-    return Point
+    def __init__(self, pat, alt):
+        alt = alt.split(',')
+        self.change = alt[0]
+        if len(alt) > 2:
+            self.index = -int(alt[1]) - 2
+            self.cut = int(alt[2]) + 1
+        else:
+            self.index = 0
+            self.cut = len(re.sub(r'[\d\.]', '', pat)) + 1
+        if pat.startswith('.'):
+            self.index -= 1
+
+    def __call__(self, val):
+        self.index += 1
+        val = int(val)
+        if val & 1:
+            return dint(val, (self.change, self.index, self.cut))
+        else:
+            return val
+
+
+class dint(int):
+    """
+    Just an int some other data can be stuck to in a data attribute.
+    call with ref=other to use the data from the other dint.
+    """
+    def __new__(cls, value, data=None, ref=None):
+        obj = int.__new__(cls, value)
+        if ref and type(ref) == dint:
+            obj.data = ref.data
+        else:
+            obj.data = data
+        return obj
 
 
 class Hyphenator(object):
@@ -60,12 +88,12 @@ class Hyphenator(object):
     def __init__(self, filename, left=2, right=2, cache=True):
         self.left = left
         self.right = right
-        if cache and filename in _hdcache:
-            self.patterns, self.cache = _hdcache[filename]
+        if cache and filename in hdcache:
+            self.patterns, self.cache = hdcache[filename]
         else:
             self.patterns, self.cache = ({}, {})
             self._readfile(filename)
-            _hdcache[filename] = (self.patterns, self.cache)
+            hdcache[filename] = (self.patterns, self.cache)
 
     def _readfile(self, filename):
         f = open(filename)
@@ -77,20 +105,19 @@ class Hyphenator(object):
             if line[0] == '%': continue
             line = line.decode(charset).strip()
             # replace ^^hh with the real character
-            pat = _re_hex(_hexrepl, line)
+            pat = parse_hex(hexrepl, line)
             # read nonstandard hyphen alternatives
             if '/' in pat:
                 pat, alt = pat.split('/', 1)
-                factory = int_alt(alt)
+                factory = parse_alt(pat, alt)
             else:
                 factory = int
-            tag, value = zip(*[(s or "", factory(i or "0"))
-                for i,s in _re_parse(pat)][:-1])
+            tag, value = zip(*[(s, factory(i or "0")) for i,s in parse(pat)])
             # chop zero's from beginning and end, and store start offset.
-            start, end = 0, -1
-            while value[start] == 0: start += 1
-            while value[end] == 0:   end -= 1
-            self.patterns[''.join(tag)] = start, value[start:(end+1 or None)]
+            start, end = 0, 0
+            while not value[start]: start += 1
+            while not value[end-1]: end -= 1
+            self.patterns[''.join(tag)] = start, value[start:(end or None)]
         f.close()
 
     def hyphenate(self, word):
@@ -109,10 +136,10 @@ class Hyphenator(object):
                     p = self.patterns.get(prepWord[i:j+1])
                     if p:
                         offset, value = p
-                        start, end = i + offset, i + offset + len(value)
-                        res[start:end] = map(max, value, res[start:end])
+                        s = slice(i + offset, i + offset + len(value))
+                        res[s] = map(max, value, res[s])
 
-            points = [i - 1 for i,r in enumerate(res) if r % 2]
+            points = [dint(i - 1, ref=r) for i,r in enumerate(res) if r % 2]
             self.cache[word] = points
 
         # correct for left and right
@@ -130,7 +157,11 @@ class Hyphenator(object):
         """
         l = list(word)
         for p in reversed(self.hyphenate(word)):
-            l[p:p] = hyphen
+            if p.data:
+                change, index, cut = p.data
+                l[p + index: p + index + cut] = change.replace('=', hyphen)
+            else:
+                l[p:p] = hyphen
         return u''.join(l)
 
     visualize = visualise
@@ -138,5 +169,5 @@ class Hyphenator(object):
 p = Hyphenator("hyph_nl.dic", left=1, right=1)
 #print repr(p.patterns)
 
-print repr(p.visualise(sys.argv[1]))
+print (p.visualise(unicode(sys.argv[1].decode('iso-8859-1'))))
 
