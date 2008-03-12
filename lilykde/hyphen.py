275161471f4240c7c3ad27d875776fb43a4c1cee
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-03-12 06:57:42 +0000

    default to the users current locale if no last used language was already set
    git-svn-id: svn://localhost/tmp/repo1/trunk@181 e61d349e-2442-45d7-aecb-3eed713e9aed

--- lilykde/hyphen.py
+++ lilykde/hyphen.py
@@ -6,6 +6,7 @@ using the hyphenator module.
 import re
 import os, os.path
 from glob import glob
+from locale import getdefaultlocale
 
 from kdecore import KConfig
 from kdeui import KInputDialog
@@ -15,8 +16,8 @@ import kate
 from lilykde.i18n import _
 from lilykde.util import py2qstringlist, qstringlist2py, runOnSelection
 
-from lilykde import config
-config = config.group("hyphenation")
+import lilykde.config
+config = lilykde.config.group("hyphenation")
 
 defaultpaths = (
     '/opt/OpenOffice.org/share/dict/ooo',
@@ -39,21 +40,40 @@ def searchDicts():
         else:
             for pref in prefixes:
                 res.append(os.path.join(pref, path))
-    paths = [p for p in res if os.path.isdir(p)]
-    # now find the hyph_xx_XX.dic files
+    paths = (p for p in res if os.path.isdir(p))
+
+    # present the user with human readable language names
     all_languages = KConfig("all_languages", True, False, "locale")
+
+    # default to the users current locale if not used before
+    try:
+        currentlang = getdefaultlocale()[0]
+    except ValueError:
+        currentlang = ""
+    defaultlang = None
+
+    # now find the hyph_xx_XX.dic files
     hyphdicts = {}
     for p in paths:
         for g in glob(os.path.join(p, 'hyph_*.dic')):
             if os.path.isfile(g):
                 lang = re.sub(r'hyph_(.*).dic', r'\1', os.path.basename(g))
+                # find a human readable name belonging to the language code
                 for i in lang, lang.split('_')[0]:
                     all_languages.setGroup(i)
                     name = unicode(all_languages.readEntry("Name"))
                     if name:
-                        lang = '%s  (%s)' % (name, lang)
+                        name = '%s  (%s)' % (name, lang)
+                        hyphdicts[name] = g
+                        # set current locale as default
+                        if lang == currentlang: defaultlang = name
                         break
-                hyphdicts[unicode(lang)] = g
+                else:
+                    hyphdicts[lang] = g
+
+    # if not used before, write the current locale (if existing) as default
+    if defaultlang and not config.readEntry("lastused") in hyphdicts:
+        config.writeEntry("lastused", defaultlang)
     return hyphdicts
 
 hyphdicts = searchDicts()
@@ -85,10 +105,10 @@ def hyphenateText(text):
     lang = askLanguage()
     if not lang: return None
     from hyphenator import Hyphenator
-    h = Hyphenator(hyphdicts[lang], 1, 1)
+    h = Hyphenator(hyphdicts[lang])
     def hyphrepl(matchObj):
         return h.inserted(matchObj.group(), ' -- ')
-    return re.compile(r'\w+', re.U).sub(hyphrepl, text)
+    return re.compile(r'[^\W0-9_]+', re.U).sub(hyphrepl, text)
 
 @runOnSelection
 def deHyphenateText(text):
