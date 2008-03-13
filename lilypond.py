c5761c86aadbb2a94f7e9ef7af007b954b15da46
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-03-13 06:29:29 +0000

    prepend our own module path to sys.path instead of extending at the end
    git-svn-id: svn://localhost/tmp/repo1/trunk@188 e61d349e-2442-45d7-aecb-3eed713e9aed

--- lilypond.py
+++ lilypond.py
@@ -7,7 +7,7 @@ import kate
 # python package named lilykde.
 # TODO: install lilykde as site package if system-wide install
 from kdecore import KStandardDirs
-sys.path.extend(map(str, KStandardDirs().findDirs("data", "lilykde")))
+sys.path[0:0] = map(unicode, KStandardDirs().findDirs("data", "lilykde"))
 
 # Get the LilyKDE version, homepage etc.
 from lilykde.about import *
