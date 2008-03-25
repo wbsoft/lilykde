bfa1ce3e6ff856a986c755967670b07479b54995
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-03-25 09:27:02 +0000

    just to save the state of the helper, it will be converted to use KProcess
    git-svn-id: svn://localhost/tmp/repo1/trunk@220 e61d349e-2442-45d7-aecb-3eed713e9aed

--- lilypond-servicemenu-helper.py
+++ lilypond-servicemenu-helper.py
@@ -1,9 +1,14 @@
 #!/python
 
 import sys, os
-from subprocess import call
+from subprocess import Popen, PIPE, STDOUT
 
-def runLilyPond(paths):
+from qt import *
+from kdecore import *
+from kdeui import *
+
+def runLilyPond(paths, log):
+    global app
     """
     Run Lilypond on a list of paths.
     If some paths are in the same directory, lilypond is run once.
@@ -11,12 +16,37 @@ def runLilyPond(paths):
     # collect the directories
     dirs = {}
     for p in paths:
-        path, file = os.path.split(p)
+        path, file = os.path.split(os.path.abspath(p))
         dirs.setdefault(path,[]).append(file)
-    
-    for path in dirs:
-        cmd = ["lilypond", "--pdf"]
-        cmd.extend(dirs[path])
-        call(cmd, cwd = path)
 
-runLilyPond(sys.argv[1:])
+    retcode = 0
+    cmd = ["lilypond", "--pdf"]
+    for path in dirs:
+        p = Popen(cmd + dirs[path], cwd=path, stdout=PIPE, stderr=STDOUT)
+        for line in p.stdout:
+            log.append(line.strip())
+            log.repaint()
+            app.processEvents()
+        retcode = max(retcode, p.wait())
+    return retcode
+
+def main():
+    global app, log
+    KCmdLineArgs.init (sys.argv, "lilypond-servicemenu-helper", "", "1.0")
+    KCmdLineArgs.addCmdLineOptions([("+files", "LilyPond files to convert")])
+    app = KApplication()
+    log = KTextBrowser()
+    app.setMainWidget(log)
+    #log.setMinimumHeight(240)
+    #log.setMinimumWidth(400)
+    log.show()
+    QTimer.singleShot(1000, run)
+    app.exec_loop()
+
+def run():
+    global app, log
+    pa = KCmdLineArgs.parsedArgs()
+    files = map(pa.arg, range(pa.count()))
+    runLilyPond(files, log)
+
+main()
