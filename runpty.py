d4b8acfba29c279392ec5efe87974aa7f004c305
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-04-12 21:10:51 +0000

    much more clean and smaller by using the subprocess module
    git-svn-id: svn://localhost/tmp/repo1/trunk@314 e61d349e-2442-45d7-aecb-3eed713e9aed

--- runpty.py
+++ runpty.py
@@ -1,86 +1,44 @@
 #!python
 """
 Runs a program within a PTY.
-Parts were taken from the pty module, but I don't want
-to connect stderr and I try to implement some signal handling.
+Some ideas were taken from the pty module, but I don't want
+to connect stderr and I implement some signal handling.
 """
 
-import sys, os, tty, pty, signal, select
+import sys, os, tty, pty, signal, select, subprocess
 
-STDIN_FILENO = 0
-STDOUT_FILENO = 1
-STDERR_FILENO = 2
-CHILD = 0
+STDIN, STDOUT = 0, 1
 
-def fork():
-    """
-    fork() -> (pid, master_fd)
-    Fork and make the child a session leader with a controlling terminal.
-    """
-    master_fd, slave_fd = pty.openpty()
-    pid = os.fork()
-    if pid == CHILD:
-        # Establish a new session.
-        os.setsid()
-        os.close(master_fd)
+master, slave = pty.openpty()
+p = subprocess.Popen(sys.argv[1:],
+    stdin = slave, stdout = slave, close_fds = True)
 
-        # Slave becomes stdin and stdout of child.
-        os.dup2(slave_fd, STDIN_FILENO)
-        os.dup2(slave_fd, STDOUT_FILENO)
-        if (slave_fd > STDERR_FILENO):
-            os.close (slave_fd)
+for s in signal.SIGINT, signal.SIGTERM:
+    signal.signal(s, lambda signalnum, frame:
+        os.kill(p.pid, signalnum))
 
-        # Explicitly open the tty to make it become a controlling tty.
-        tmp_fd = os.open(os.ttyname(STDOUT_FILENO), os.O_RDWR)
-        os.close(tmp_fd)
+try:
+    mode = tty.tcgetattr(STDIN)
+    tty.setraw(STDIN)
+    restore = 1
+except tty.error:
+    restore = 0
+
+while p.poll() is None:
+    try:
+        fds = select.select([master, STDIN], [], [])[0]
+    except select.error:
+        pass
     else:
-        os.close(slave_fd)
-    # Parent and child process.
-    return pid, master_fd
+        if master in fds:
+            os.write(STDOUT, os.read(master, 1024))
+        if STDIN in fds:
+            data = os.read(STDIN, 1024)
+            while data:
+                n = os.write(master, data)
+                data = data[n:]
 
-def copy_data(master_fd):
-    """
-    Parent copy loop.
-    Copies pty master -> standard output and standard input -> pty master
-    """
-    while True:
-        try:
-            rfds = select.select([master_fd, STDIN_FILENO], [], [])[0]
-        except select.error:
-            # just call again in case of EINTR
-            pass
-        else:
-            if master_fd in rfds:
-                data = os.read(master_fd, 1024)
-                os.write(STDOUT_FILENO, data)
-            if STDIN_FILENO in rfds:
-                data = os.read(STDIN_FILENO, 1024)
-                while data != '':
-                    n = os.write(master_fd, data)
-                    data = data[n:]
-
-def spawn(argv, signals=()):
-    """Create a spawned process."""
-    pid, master_fd = fork()
-    if pid == CHILD:
-        os.execlp(argv[0], *argv)
-    # install signal handler that passes the signal to the child
-    def handler(signalnum, frame):
-        os.kill(pid, signalnum)
-    for s in signals:
-        signal.signal(s, handler)
-    try:
-        mode = tty.tcgetattr(STDIN_FILENO)
-        tty.setraw(STDIN_FILENO)
-        restore = 1
-    except tty.error:    # This is the same as termios.error
-        restore = 0
-    try:
-        copy_data(master_fd)
-    except (IOError, OSError):
-        if restore:
-            tty.tcsetattr(STDIN_FILENO, tty.TCSAFLUSH, mode)
-    os.close(master_fd)
-
-if __name__ == "__main__":
-    spawn(sys.argv[1:], (2, 15))
+if restore:
+    tty.tcsetattr(STDIN, tty.TCSAFLUSH, mode)
+os.close(master)
+sys.exit(p.poll())
