af5382f3c3cf4d120434c018cb8da09d279cdeaa
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-04-27 14:25:11 +0000

    version bump for trunk: 0.6.x. Next planned feature: score setup wizard.
    git-svn-id: svn://localhost/tmp/repo1/trunk@335 e61d349e-2442-45d7-aecb-3eed713e9aed

---
+++ lilykde/scorewiz.py
@@ -0,0 +1,40 @@
+"""
+A Score Wizard
+
+provides a wizard dialog to quickly set up a LilyPond score.
+
+The user can select items (staffs or different types of staffgroup)
+and define their order. All items will live in Score, i.e.
+
+general preambule (main settings, title etc.)
+preambule of item1 (assignments etc.)
+preambule of item2
+
+\score {
+  <<
+    music of item1
+    music of item2
+  >>
+  \layout {
+    general layout stuff
+  }
+  optional midi block
+}
+
+aftermath of item1 (additional score blocks,)
+aftermath of item2 (e.g. for extra midi output files)
+
+
+Wizard:
+
+    Page 1:
+
+        Titling, etc.
+
+    page 2:
+
+        Parts with associated settings
+
+
+
+"""
