3eb8bf98dc4863885e2e1a866e5243ae2ed045a1
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-04-27 20:10:29 +0000

    start work on header wizard. will work with html preview
    git-svn-id: svn://localhost/tmp/repo1/trunk@336 e61d349e-2442-45d7-aecb-3eed713e9aed

--- lilykde/scorewiz.py
+++ lilykde/scorewiz.py
@@ -38,3 +38,53 @@ Wizard:
 
 
 """
+
+from string import Template
+
+# Translate messages
+from lilykde.i18n import _
+
+headers = (
+    ('dedication',  _("Dedication")),
+    ('title',       _("Title")),
+    ('subtitle',    _("Subtitle")),
+    ('subsubtitle', _("Subsubtitle")),
+    ('instrument',  _("Instrument")),
+    ('composer',    _("Composer")),
+    ('arranger',    _("Arranger")),
+    ('poet',        _("Poet")),
+    ('meter',       _("Meter")),
+    ('piece',       _("Piece")),
+    ('opus',        _("Opus")),
+    ('copyright',   _("Copyright")),
+    ('tagline',     _("Tagline")),
+)
+
+html = Template(r"""<table width=360 style='font-family: serif;'>
+<tr><td colspan=3 align=center>$dedication</td><tr>
+<tr><td colspan=3 align=center style='font-size:20pt;'><b>$title</b></td><tr>
+<tr><td colspan=3 align=center style='font-size:12pt;'><b>$subtitle</b></td><tr>
+<tr><td colspan=3 align=center><b>$subsubtitle</b></td><tr>
+<tr>
+    <td align=left width='25%%'>$poet</td>
+    <td align=center><b>$instrument</b></td>
+    <td align=right width='25%%'>$composer</td>
+</tr>
+<tr>
+    <td align=left>$meter</td>
+    <td> </td>
+    <td align=right>$arranger</td>
+</tr>
+<tr>
+    <td align=left>$piece</td>
+    <td> </td>
+    <td align=right>$opus</td>
+</tr>
+<tr><td colspan=3 align=center><img src='scorewiz.png'></td></tr>
+<tr><td colspan=3 align=center>$copyright <i>(%s)</i></td></tr>
+<tr><td colspan=3 align=center>$tagline <i>(%s)</i></td></tr>
+</table>""").substitute(
+    dict((k, "<a href='#%s'>%s</a>" % (k, v)) for k, v in headers)) % (
+        _("bottom of first page"),
+        _("bottom of last page"))
+
