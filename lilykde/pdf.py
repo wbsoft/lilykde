9b471e7fd49fc3b9e11fcf6865613a88f9bb1ac3
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-05-12 05:17:42 +0000

    try to remember the scroll position in the pdf between updates. This is actually a workaround for KPDF loosing the watch and forgetting the scroll position of a PDF document due to LilyPond deleting the old PDF first and then creating a new one with a different inode number.
    git-svn-id: svn://localhost/tmp/repo1/trunk@406 e61d349e-2442-45d7-aecb-3eed713e9aed

--- lilykde/pdf.py
+++ lilykde/pdf.py
@@ -3,8 +3,9 @@
 import kate
 import kate.gui
 
-from qt import QWidget
-from kdecore import KURL
+from qt import QWidget, QTimer
+from dcopext import DCOPApp
+from kdecore import KApplication, KURL
 from kparts import createReadOnlyPart
 
 # Translate the messages
@@ -19,14 +20,35 @@ tool.show()
 show = tool.show
 hide = tool.hide
 
-#_file = ""
+_file = ""
 
 def openFile(pdf):
-    #if _file != pdf:
-        # KPDF does not always watch the file for updates if the inode
-        # number changes, which LilyPond does...
-        pdfpart.openURL(KURL(pdf))
-        #_file = pdf
+    """ Open the specified PDF document """
+
+    global _file
+
+    c = KApplication.kApplication().dcopClient()
+    kpdf = DCOPApp(c.appId(), c).kpdf
+
+    # When LilyPond writes a PDF, it first deletes the old one.
+    # So the new PDF gets a different inode number, which causes
+    # KPDF to sometimes loose the 'watch' on the file.
+    # So we call KPDF to open the file, and remember the page number
+    # displayed ourselves, because KPDF also seems to forget the scroll
+    # position due to LilyPond deleting the old PDF first.
+    # It would be best that either KPDF is fixed to just look for a
+    # named file, even if it has a different inode number, or that
+    # LilyPond is fixed to not delete the old PDF first, but just
+    # truncate it and write the new data into it.
+
+    # keep the current page number
+    page = kpdf.currentPage()[1]
+    # KPDF does not always watch the file for updates if the inode
+    # number changes, which LilyPond does...
+    kpdf.openDocument(KURL(pdf))
+    if _file == pdf:
+        QTimer.singleShot(100, lambda: kpdf.goToPage(page))
+    _file = pdf
 
 
 # kate: indent-width 4;
