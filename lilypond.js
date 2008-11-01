6537ced64cc947f90a7f03600983470d26b3ffd1
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-11-01 21:08:57 +0000

    add LilyPond indenter for KatePart
    git-svn-id: svn://localhost/tmp/repo1/trunk@625 e61d349e-2442-45d7-aecb-3eed713e9aed

---
+++ lilypond.js
@@ -0,0 +1,41 @@
+/** kate-script
+ * name: LilyPond
+ * license: LGPL
+ * author: Wilbert Berendsen <info@wilbertberendsen.nl>
+ * version: 1
+ * kate-version: 3.0
+ * type: indentation
+ */
+
+var triggerCharacters = "}>";
+
+reOpener = /\{|\<\</g;
+reCloser = /\}|\>\>/g;
+reStartClosers = /^(\s*(\}|\>\>))+/
+spaceLine = /^\s*$/;
+
+function indent(line, indentWidth, ch)
+{
+  // not necessary to indent the first line
+  if (line == 0)
+    return -2;
+
+  // search backwards for first non-space line.
+  var prev = line;
+  while ((prev -= 1) >= 0) {
+    if (!document.line(prev).match(spaceLine)) {
+      var c = document.line(line); //current line
+      var p = document.line(prev); //previous non-space line
+      var oldIndent = document.firstVirtualColumn(prev);
+      // count the number of openers and closers in the previous line,
+      // discarding first closers.
+      p = p.replace(reStartClosers, '');
+      var delta = p.match(reOpener).length - p.match(reCloser).length;
+      // now count the number of closers in the beginning of the current line.
+      if (m = c.match(reStartClosers))
+	delta -= m[0].match(reCloser).length;
+      return Math.max(0, oldIndent + delta * indentWidth);
+    }
+  }
+  return 0;
+}
