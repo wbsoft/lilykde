3c7ae3f169f90a0a654838e357e5a1149af7313b
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-12-05 23:10:54 +0000

    add lilydom
    git-svn-id: svn://localhost/tmp/repo1/trunk@824 e61d349e-2442-45d7-aecb-3eed713e9aed

---
+++ frescobaldi/python/ly/pitch.py
@@ -0,0 +1,84 @@
+# This file is part of the Frescobaldi project, http://www.frescobaldi.org/
+#
+# Copyright (c) 2008  Wilbert Berendsen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# See http://www.gnu.org/licenses/ for more information.
+
+# pitches
+class pitchwriter(object):
+    def __init__(self, names, accs, special = None):
+        self.names = names
+        self.accs = accs
+        self.special = special or ()
+
+    def __call__(self, note, alter = 0):
+        pitch = self.names[note]
+        if alter:
+            acc = self.accs[int(alter * 4 + 4)]
+            # FIXME: warn if no alter defined
+            pitch += acc
+        for s, r in self.special:
+            pitch = pitch.replace(s, r)
+        return pitch
+
+
+pitchInfo = {
+    'nederlands': (
+        ('c','d','e','f','g','a','b'),
+        ('eses', 'eseh', 'es', 'eh', '', 'ih','is','isih','isis'),
+        (('ees', 'es'), ('aes', 'as'))
+    ),
+    'english': (
+        ('c','d','e','f','g','a','b'),
+        ('ff', 'tqf', 'f', 'qf', '', 'qs', 's', 'tqs', 'ss'),
+        (('flat', 'f'), ('sharp', 's'))
+    ),
+    'deutsch': (
+        ('c','d','e','f','g','a','h'),
+        ('eses', 'eseh', 'es', 'eh', '', 'ih','is','isih','isis'),
+        (('ees', 'es'), ('aes', 'as'), ('hes', 'b'))
+    ),
+    'svenska': (
+        ('c','d','e','f','g','a','h'),
+        ('essess', '', 'ess', '', '', '','iss','','ississ'),
+        (('ees', 'es'), ('aes', 'as'), ('hess', 'b'))
+    ),
+    'italiano': (
+        ('do', 're', 'mi', 'fa', 'sol', 'la', 'si'),
+        ('bb', 'bsb', 'b', 'sb', '', 'sd', 'd', 'dsd', 'dd')
+    ),
+    'espanol': (
+        ('do', 're', 'mi', 'fa', 'sol', 'la', 'si'),
+        ('bb', '', 'b', '', '', '', 's', '', 'ss')
+    ),
+    'portuges': (
+        ('do', 're', 'mi', 'fa', 'sol', 'la', 'si'),
+        ('bb', 'btqt', 'b', 'bqt', '', 'sqt', 's', 'stqt', 'ss')
+    ),
+    'vlaams': (
+        ('do', 're', 'mi', 'fa', 'sol', 'la', 'si'),
+        ('bb', '', 'b', '', '', '', 'k', '', 'kk')
+    ),
+}
+
+pitchInfo['norsk'] = pitchInfo['deutsch']
+pitchInfo['suomi'] = pitchInfo['deutsch']
+pitchInfo['catalan'] = pitchInfo['italiano']
+
+
+pitchNames = dict(
+    (lang, pitchwriter(*data)) for lang, data in pitchInfo.iteritems())
+
