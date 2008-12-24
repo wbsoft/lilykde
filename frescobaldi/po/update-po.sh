c0342c2ee07bfa4ae0d6a4a58accce30d54d2241
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-12-24 11:17:07 +0000

    include package info in pot
    git-svn-id: svn://localhost/tmp/repo1/trunk@906 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/po/update-po.sh
+++ frescobaldi/po/update-po.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+package=$(sed -n 's/^project\s*(\s*\(.*\)\s*).*/\1/p' ../CMakeLists.txt)
+version=$(sed -n 's/.*VERSION "\(.*\)".*/\1/p' ../CMakeLists.txt)
+email=$(sed -n 's/^bugs\s*=\s*"\(.*\)".*/\1/p' ../frescobaldi.py)
+
 # Update pot file:
-xgettext --keyword=i18n --keyword=ki18n --keyword=I18N_NOOP \
+xgettext \
+    --package-name="$package" \
+    --package-version="$version" \
+    --msgid-bugs-address="$email" \
+    --keyword=i18n --keyword=ki18n --keyword=I18N_NOOP \
     --output=frescobaldi.pot \
     --language=python \
     dummy.py \
