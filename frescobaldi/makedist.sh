a2a864daa377085bf493b24f6d5b54b1c364c8ad
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-12-26 00:15:54 +0000

    Bump version to 0.7 (in line with LilyKDE3) add makedist script that prebuilts icons etc for releases.
    git-svn-id: svn://localhost/tmp/repo1/trunk@916 e61d349e-2442-45d7-aecb-3eed713e9aed

---
+++ frescobaldi/makedist.sh
@@ -0,0 +1,34 @@
+#/bin/sh
+package=$(sed -n 's/^project\s*(\s*\(.*\)\s*).*/\1/p' CMakeLists.txt)
+version=$(sed -n 's/.*VERSION "\(.*\)".*/\1/p' CMakeLists.txt)
+
+pkg="$package-$version"
+
+die()
+{
+  local msg=$1
+  echo "makedist: error: $msg" >&2
+  exit 1
+}
+
+echo Creating $pkg.tar.gz
+svn export . $pkg || die "export failed"
+cd "$pkg"
+mkdir build
+cd build
+cmake .. || die "cmake in export failed"
+make -C po || die "make failed in po/"
+make -C pics || die "make failed in pics/"
+# remove most cmake generated files
+find . -maxdepth 2 \
+  \( -type d -a -name CMakeFiles \) \
+  -o -name cmake*.cmake \
+  -o -name CMakeCache.txt \
+  -o -name Makefile \
+  | xargs rm -fr
+rm -f frescobaldi data python
+cd ../..
+tar zcf $pkg.tar.gz $pkg || die "making tarball failed"
+rm -fr $pkg 
+echo Done.
+
