3e5b3410c2520258675d70479e6548df88da226f
Wilbert Berendsen <wbsoft@xs4all.nl>
2008-12-26 12:20:12 +0000

    add readme and do not prebuild anything, it only causes problems :-(
    git-svn-id: svn://localhost/tmp/repo1/trunk@920 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/makedist.sh
+++ frescobaldi/makedist.sh
@@ -13,22 +13,9 @@ die()
 
 echo Creating $pkg.tar.gz
 svn export . $pkg || die "export failed"
-cd "$pkg"
-# make sure pot is up-to-date
-cd po
-./update-po.sh
-cd ..
-mkdir build2
-cd build2
-cmake .. || die "cmake failed"
-make || die "make failed"
-# create clean build dir and copy over .png and .mo files
-cd ..
-mkdir build build/po build/pics
-cp -a build2/po/*.mo build/po/
-cp -a build2/pics/*.png build/pics/
-rm -fr build2
-cd ..
+# ensure pot is up-to-date
+( cd "$pkg/po" && ./update-po.sh ; )
+# create tarball
 tar zcf $pkg.tar.gz $pkg || die "making tarball failed"
 rm -fr $pkg 
 echo Done.
