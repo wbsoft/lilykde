470b74224efd70037e79851e2d8c0d1517773865
Wilbert Berendsen <wbsoft@xs4all.nl>
2009-01-09 12:18:43 +0000

    Lucky me! I found out about CMAKE_SKIP_RULE_DEPENDENCY. No need to make a prebuilt directory. Just build the icons and translations for the release tarballs and put them in the source tree. When Frescobaldi is built from a tarball inside the source tree, the prebuilt icons and translations are used. Which speeds up the install process and removes the build dependency on ImageMagick and Gettext (and even LilyPond).
    git-svn-id: svn://localhost/tmp/repo1/trunk@998 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/makedist.sh
+++ frescobaldi/makedist.sh
@@ -14,9 +14,7 @@ die()
 echo Creating $pkg.tar.gz
 svn export . $pkg || die "export failed"
 
-# now build the po and pics directories and put the resulting MO
-# and PNG files in a "prebuilt" directory. If that directory is present,
-# released tar balls will not try to rebuild the LilyPond icons and MO files.
+# Prebuild the icons and mo files
 cd "$pkg" || die "could not cd into package"
 
 # ensure pot is up-to-date
@@ -28,41 +26,16 @@ cd "$pkg" || die "could not cd into package"
   make
 ) || die "could not build package"
 
-mkdir prebuilt prebuilt/pics prebuilt/po
-# put mo files in prebuilt
-cp build/po/*.mo prebuilt/po/
-# put pics (LilyPond-generated icons) in prebuilt
-cp build/pics/*.png prebuilt/pics/
+# put mo files in source tree
+cp build/po/*.mo po/
+# put pics (LilyPond-generated icons) in source tree
+cp build/pics/*.png pics/
 
-# Put a CMakeLists.txt file in the prebuilt directory
-cat <<"EOF" > prebuilt/CMakeLists.txt
-# These are rules to install prebuilt MO and PNG files. This
-# has been done to make installing release tarballs easier.
-# This directory and its contents have been created by the
-# makedist.sh script, and are not part of the Frescobaldi
-# SVN repository. To rebuild these files just remove the
-# "prebuilt" directory and run cmake again.
-
-# Message Object files (generated from ../po/*.po)
-file(GLOB _mo_files po/*.mo)
-foreach(_mo ${_mo_files})
-  get_filename_component(_lang ${_mo} NAME_WE)
-  install(
-    FILES ${_mo}
-    DESTINATION ${LOCALE_INSTALL_DIR}/${_lang}/LC_MESSAGES
-    RENAME ${PROJECT_NAME}.mo
-  )
-endforeach(_mo)
-
-# LilyPond icons (generated from ../pics/*.ly)
-file(GLOB _png_files pics/*.png)
-install(FILES ${_png_files} DESTINATION ${APP_DIR}/pics)
-EOF
-
-# Back to parent directory
-cd ..
+# Remove build directory
+rm -fr build
 
 # Create tarball
+cd ..
 tar zcf $pkg.tar.gz $pkg || die "making tarball failed"
 rm -fr $pkg 
 echo Done.
