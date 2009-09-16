6b14018f22c226fc5b2e88232dd98e0b8443626e
Wilbert Berendsen <wbsoft@xs4all.nl>
2009-09-16 09:07:50 +0000

    it didnt work...
    git-svn-id: svn://localhost/tmp/repo1/trunk@1442 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/python/frescobaldi_app/lqi.py
+++ frescobaldi/python/frescobaldi_app/lqi.py
@@ -95,7 +95,7 @@ class Articulations(Lqi):
                 b = QToolButton(self)
                 b.setAutoRaise(True)
                 # load and convert the icon to the default text color
-                b.setIcon(LilyIcon('articulation_%s' % sign))
+                b.setIcon(LilyIcon('articulation_%s' % sign, 22))
                 b.setIconSize(QSize(22, 22))
                 b.setToolTip('%s (\\%s)' % (title, sign))
                 QObject.connect(b, SIGNAL("clicked()"),
