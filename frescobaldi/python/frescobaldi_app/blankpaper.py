a0d9765aa7af212fed1426b7482255b01a437a3b
Wilbert Berendsen <wbsoft@xs4all.nl>
2009-10-27 14:15:47 +0000

    add highly configurable custom staff type
    git-svn-id: svn://localhost/tmp/repo1/trunk@1561 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/python/frescobaldi_app/blankpaper.py
+++ frescobaldi/python/frescobaldi_app/blankpaper.py
@@ -20,6 +20,7 @@
 """
 A wizard to create empty staff paper with LilyPond
 """
+import sip
 
 from PyQt4.QtCore import QObject, QSize, Qt, SIGNAL
 from PyQt4.QtGui import (
@@ -216,7 +217,7 @@ class Dialog(KDialog):
             output.append('oddHeaderMarkup = \\markup \\strut')
         output.append('evenHeaderMarkup = ##f')
         output.append('oddFooterMarkup = \\markup \\fill-line {')
-        output.append('\\sans \\fontsize #-8 { %s }' % "FRESCOBALDI.ORG")
+        output.append('\\sans \\abs-fontsize #6 { %s }' % "FRESCOBALDI.ORG")
         output.append('\\null')
         output.append('}')
         output.append('head-separation = 3\\mm')
@@ -224,6 +225,7 @@ class Dialog(KDialog):
         output.append('top-margin = 10\\mm')
         output.append('bottom-margin = 10\\mm')
         output.append('ragged-last-bottom = ##f')
+        output.append('ragged-right = ##f')
         output.append('}\n')
         # music expression
         output.append('music = \\repeat unfold %d { %% pages' % self.pageCount.value())
@@ -354,6 +356,7 @@ class LayoutContexts(object):
         self._contexts = {}
         self._staffContexts = ['Staff']
         self._spanBarContexts = []
+        self._alwaysShowSystemStartBar = False
     
     def setStaffContexts(self, staffContexts):
         """
@@ -361,12 +364,27 @@ class LayoutContexts(object):
         """
         self._staffContexts = staffContexts
 
+    def addStaffContext(self, staffContext):
+        """
+        Just add a single context that must get (in)visible barlines etc.
+        """
+        if staffContext not in self._staffContexts:
+            self._staffContexts.append(staffContext)
+
     def setSpanBarContexts(self, spanBarContexts):
         """
         Specify which contexts must have the Span_bar_engraver removed
         if no barlines are requested.
         """
         self._spanBarContexts = spanBarContexts
+    
+    def addSpanBarContext(self, spanBarContext):
+        """
+        Just add a single context which must have the Span_bar_engraver
+        removed...
+        """
+        if spanBarContext not in self._spanBarContexts:
+            self._spanBarContexts.append(spanBarContext)
         
     def add(self, context, line):
         """ Add a line to the given context """
@@ -381,12 +399,17 @@ class LayoutContexts(object):
         for c in self._spanBarContexts:
             self.add(c, '\\remove "Span_bar_engraver"')
     
+    def setAlwaysShowSystemStartBar(self, show=True):
+        self._alwaysShowSystemStartBar = show
+        
     def ly(self):
         """ Return a list of LilyPond lines. """
         result = []
         for name, lines in self._contexts.iteritems():
             result.append('\\context {')
             result.append('\\%s' % name)
+            if name == 'Score' and self._alwaysShowSystemStartBar:
+                result.append("\override SystemStartBar #'collapse-height = #1")
             result.extend(lines)
             result.append('}')
         return result
@@ -435,7 +458,7 @@ class SingleStaff(StaffBase):
             return ['\\new TabStaff { \\music }']
         else:
             if self.clef.clef():
-                return ['\\new Staff { \\clef %s \\music }' % self.clef.clef()]
+                return ['\\new Staff { \\clef "%s" \\music }' % self.clef.clef()]
             else:
                 layout.add('Staff', '\\remove "Clef_engraver"')
                 return ['\\new Staff { \\music }']
@@ -577,7 +600,7 @@ class ChoirStaff(StaffBase):
         return music
 
 from PyQt4.QtCore import QTimer
-from PyQt4.QtGui import QToolButton, QTreeWidget, QTreeWidgetItem
+from PyQt4.QtGui import QPushButton, QToolBar, QToolButton, QTreeWidget, QTreeWidgetItem
 from PyKDE4.kdeui import KMenu, KPushButton, KStandardGuiItem
 from frescobaldi_app.mainapp import SymbolManager
 
@@ -585,39 +608,104 @@ class CustomStaff(SymbolManager, QWidget):
     def __init__(self, dialog):
         QWidget.__init__(self, dialog)
         SymbolManager.__init__(self)
-        self.setDefaultSymbolSize(48)
+        self.setDefaultSymbolSize(40)
         self.dialog = dialog
         self.setLayout(QGridLayout())
         self.tree = QTreeWidget()
         self.stack = QStackedWidget()
+        StackFader(self.stack)
         self.systems = QSpinBox()
-        self.layout().addWidget(self.tree, 0, 0)
-        self.layout().addWidget(self.stack, 0, 1, 1, 2)
-        self.layout().addWidget(self.systems, 1, 2)
+        newStaves = QVBoxLayout()
+        operations = QHBoxLayout()
+        self.layout().addLayout(newStaves, 0, 0)
+        self.layout().addWidget(self.tree, 0, 1)
+        self.layout().addWidget(self.stack, 0, 2, 1, 2)
+        self.layout().addWidget(self.systems, 1, 3)
         self.systems.setRange(1, 64)
         l = QLabel(i18n("Systems per page:"))
         l.setBuddy(self.systems)
-        self.layout().addWidget(l, 1, 1, Qt.AlignRight)
+        self.layout().addWidget(l, 1, 2, Qt.AlignRight)
+        self.layout().addLayout(operations, 1, 1)
+        
+        operations.setSpacing(0)
+        operations.setContentsMargins(0, 0, 0, 0)
+        removeButton = KPushButton(KStandardGuiItem.remove())
+        operations.addWidget(removeButton)
+        upButton = QToolButton()
+        upButton.setIcon(KIcon("go-up"))
+        operations.addWidget(upButton)
+        downButton = QToolButton()
+        downButton.setIcon(KIcon("go-down"))
+        operations.addWidget(downButton)
+        QObject.connect(removeButton, SIGNAL("clicked()"), self.removeSelectedItems)
+        QObject.connect(upButton, SIGNAL("clicked()"), self.moveSelectedItemsUp)
+        QObject.connect(downButton, SIGNAL("clicked()"), self.moveSelectedItemsDown)
+        newStaves.setSpacing(0)
+        newStaves.setContentsMargins(0, 0, 0, 0)
+        
+        self.tree.setIconSize(QSize(32, 32))
+        self.tree.setDragDropMode(QTreeWidget.InternalMove)
         self.tree.headerItem().setHidden(True)
-        layout = QHBoxLayout()
-        self.layout().addLayout(layout, 1, 0)
-        self.addButton = KPushButton(KStandardGuiItem.add())
-        layout.addWidget(self.addButton)
-        self.removeButton = QToolButton()
-        self.removeButton.setIcon(KIcon('list-remove'))
-        layout.addWidget(self.removeButton)
-        self.upButton = QToolButton()
-        self.upButton.setIcon(KIcon('go-up'))
-        layout.addWidget(self.upButton)
-        self.downButton = QToolButton()
-        self.downButton.setIcon(KIcon('go-down'))
-        layout.addWidget(self.downButton)
-        menu = KMenu(self)
-        self.addButton.setMenu(menu)
-        a = menu.addAction(i18n("Bracket Group"))
-        self.addSymbol(a, 'system_bracket')
-        a = menu.addAction(i18n("Brace Group"))
-        self.addSymbol(a, 'system_brace')
+        QObject.connect(self.tree, SIGNAL("itemSelectionChanged()"), self.slotSelectionChanged)
+        
+        for staffType in (
+            BracketItem,
+            BraceItem,
+            StaffItem,
+            ):
+            b = QPushButton(staffType.name())
+            b.setIconSize(QSize(40, 40))
+            QObject.connect(b, SIGNAL("clicked()"),
+                lambda t=staffType: self.createItem(t))
+            self.addSymbol(b, staffType.symbol())
+            newStaves.addWidget(b)
+            
+        
+    def createItem(self, staffType):
+        empty = self.tree.topLevelItemCount() == 0
+        items = self.tree.selectedItems()
+        if len(items) == 1 and items[0].flags() & Qt.ItemIsDropEnabled:
+            parent = items[0]
+        else:
+            parent = self.tree
+        item = staffType(self, parent)
+        if empty:
+            item.setSelected(True)
+        
+    def slotSelectionChanged(self):
+        items = self.tree.selectedItems()
+        if items:
+            self.stack.setCurrentWidget(items[0].widget)
+        
+    def moveSelectedItemsUp(self):
+        items = self.tree.selectedItems()
+        if items:
+            item = items[0] # currently we support only one selected item
+            parent = item.parent() or self.tree.invisibleRootItem()
+            index = parent.indexOfChild(item)
+            if index > 0:
+                parent.takeChild(index)
+                parent.insertChild(index - 1, item)
+                self.tree.clearSelection()
+                item.setSelected(True)
+                item.setExpanded(True)
+    
+    def moveSelectedItemsDown(self):
+        items = self.tree.selectedItems()
+        if items:
+            item = items[0] # currently we support only one selected item
+            parent = item.parent() or self.tree.invisibleRootItem()
+            index = parent.indexOfChild(item)
+            if index < parent.childCount() - 1:
+                parent.takeChild(index)
+                parent.insertChild(index + 1, item)
+                self.tree.clearSelection()
+                item.setSelected(True)
+                item.setExpanded(True)
+    
+    def removeSelectedItems(self):
+        for item in self.tree.selectedItems():
+            item.remove()
 
     def systemCount(self):
         return self.systems.value()
@@ -627,5 +715,181 @@ class CustomStaff(SymbolManager, QWidget):
         
     def default(self):
         self.tree.clear()
+        self.systems.setValue(4)
         
+    def music(self, layout):
+        music = ['<<']
+        for i in range(self.tree.topLevelItemCount()):
+            music.extend(self.tree.topLevelItem(i).music(layout))
+        music.append('>>')
+        return music
         
+    def staffCount(self):
+        count = 0
+        for i in range(self.tree.topLevelItemCount()):
+            count += self.tree.topLevelItem(i).staffCount()
+        return count
+        
+
+class Item(QTreeWidgetItem):
+    def __init__(self, staff, parent):
+        self.staff = staff
+        QTreeWidgetItem.__init__(self, parent)
+        self.setText(0, self.name())
+        self.setIcon(0, staff.symbolIcon(self.symbol()))
+        self.setExpanded(True)
+        self.widget = self.createWidget()
+        staff.stack.addWidget(self.widget)
+        
+    def createWidget(self):
+        return QWidget()
+        
+    def remove(self):
+        while self.childCount():
+            self.child(0).remove()
+        sip.delete(self.widget)
+        sip.delete(self)
+
+    def childMusic(self, layout):
+        """ Returns a list with music from all children """
+        music = []
+        for i in range(self.childCount()):
+            music.extend(self.child(i).music(layout))
+        return music
+
+    def staffCount(self):
+        """ Returns the number of real staves """
+        count = 0
+        for i in range(self.childCount()):
+            count += self.child(i).staffCount()
+        return count
+
+
+class StaffItem(Item):
+    def __init__(self, staff, parent):
+        Item.__init__(self, staff, parent)
+        self.setFlags(self.flags() & ~Qt.ItemIsDropEnabled)
+        
+    @staticmethod
+    def name():
+        return i18n("Staff")
+    
+    @staticmethod
+    def symbol():
+        return 'clef_none'
+
+    def createWidget(self):
+        w = QWidget()
+        w.setLayout(QGridLayout())
+        self.clef = ClefSelector(noclef=True, tab=True)
+        l = QLabel(i18n("Clef:"))
+        l.setBuddy(self.clef)
+        w.layout().addWidget(l, 0, 0, Qt.AlignRight)
+        w.layout().addWidget(self.clef, 0, 1)
+        QObject.connect(self.clef, SIGNAL("currentIndexChanged(int)"), self.clefChanged)
+        self.spaceAbove = QSpinBox()
+        self.spaceAbove.setRange(0, 25)
+        self.spaceAbove.setValue(5)
+        self.spaceBelow = QSpinBox()
+        self.spaceBelow.setRange(0, 25)
+        self.spaceBelow.setValue(5)
+        w.layout().addWidget(self.spaceAbove, 1, 1)
+        w.layout().addWidget(self.spaceBelow, 2, 1)
+        l = QLabel(i18n("Space above:"))
+        l.setBuddy(self.spaceAbove)
+        w.layout().addWidget(l, 1, 0, Qt.AlignRight)
+        l = QLabel(i18n("Space below:"))
+        l.setBuddy(self.spaceBelow)
+        w.layout().addWidget(l, 2, 0, Qt.AlignRight)
+        
+        return w
+        
+    def clefChanged(self, index):
+        self.setIcon(0, self.staff.symbolIcon('clef_%s' % (self.clef.clef() or 'none')))
+
+    def staffCount(self):
+        return 1
+    
+    def music(self, layout):
+        clef = self.clef.clef()
+        if clef == 'tab':
+            staff = 'TabStaff'
+        else:
+            staff = 'Staff'
+        layout.addStaffContext(staff)
+        music = ['\\new %s \\with {' % staff]
+        music.append(
+            "\\override VerticalAxisGroup #'minimum-Y-extent = #'(-%d . %d)" %
+            (self.spaceBelow.value(), self.spaceAbove.value()))
+        if not clef:
+            music.append('\\remove "Clef_engraver"')
+        if not clef or clef == 'tab':
+            music.append('} { \\music }')
+        else:
+            music.append('} { \\clef "%s" \\music }' % clef)
+        return music
+
+
+class BraceItem(Item):
+    
+    @staticmethod
+    def name():
+        return i18n("Brace")
+    
+    @staticmethod
+    def symbol():
+        return 'system_brace'
+
+    def music(self, layout):
+        layout.addSpanBarContext('PianoStaff')
+        withMusic = []
+        if self.staffCount() == 1:
+            withMusic.append("\override SystemStartBrace #'collapse-height = #1")
+            layout.setAlwaysShowSystemStartBar()
+        if withMusic:
+            music = ['\\new PianoStaff \\with {'] + withMusic + ['} <<']
+        else:
+            music = ['\\new PianoStaff <<']
+        music.extend(self.childMusic(layout))
+        music.append('>>')
+        return music
+
+
+class BracketItem(Item):
+    
+    @staticmethod
+    def name():
+        return i18n("Bracket")
+    
+    @staticmethod
+    def symbol():
+        return 'system_bracket'
+
+    def createWidget(self):
+        w = QWidget()
+        w.setLayout(QVBoxLayout())
+        self.squareBracket = QCheckBox(i18n("Square Bracket"))
+        w.layout().addWidget(self.squareBracket)
+        self.connectBarLines = QCheckBox(i18n("Connect Barlines"))
+        self.connectBarLines.setChecked(True)
+        w.layout().addWidget(self.connectBarLines)
+        return w
+        
+    def music(self, layout):
+        staff = self.connectBarLines.isChecked() and 'StaffGroup' or 'ChoirStaff'
+        layout.addSpanBarContext(staff)
+        withMusic = []
+        if self.staffCount() == 1:
+            withMusic.append("\override SystemStartBracket #'collapse-height = #1")
+            layout.setAlwaysShowSystemStartBar()
+        if self.squareBracket.isChecked():
+            withMusic.append("systemStartDelimiter = #'SystemStartSquare")
+        if withMusic:
+            music = ['\\new %s \\with {' % staff] + withMusic + ['} <<']
+        else:
+            music = ['\\new %s <<' % staff]
+        music.extend(self.childMusic(layout))
+        music.append('>>')
+        return music
+
+
