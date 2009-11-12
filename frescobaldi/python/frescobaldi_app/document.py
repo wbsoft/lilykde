973e3167d1b1673c011cc0eddcc314945c62dab2
Wilbert Berendsen <wbsoft@xs4all.nl>
2009-11-12 10:56:35 +0000

    correct module for exception
    git-svn-id: svn://localhost/tmp/repo1/trunk@1621 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/python/frescobaldi_app/document.py
+++ frescobaldi/python/frescobaldi_app/document.py
@@ -664,7 +664,7 @@ class DocumentManipulator(object):
         text, start = self.doc.selectionOrDocument()
         try:
             ly.tools.absoluteToRelative(text, start).applyToCursor(EditCursor(self.doc.doc))
-        except ly.tools.NoExpressionFound:
+        except ly.NoExpressionFound:
             KMessageBox.error(self.doc.app.mainwin, i18n(
                 "Please select a music expression, enclosed in << ... >> or { ... }."))
 
