0ee32be3336d208aefd668fbf75191f6a74c3176
Wilbert Berendsen <wbsoft@xs4all.nl>
2010-07-23 05:50:28 +0000

    beginnings of a run script that starts Frescobaldi without requiring CMake and make and without installing
    git-svn-id: svn://localhost/tmp/repo1/trunk@2085 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/checkmodules.py
+++ frescobaldi/checkmodules.py
@@ -5,56 +5,69 @@
 
 import sys, os
 
-missing = []
+def mkver(major, minor, release):
+    return major * 65536 + minor * 256 + release
 
-# these are checks for modules Frescobaldi needs, but you can tailor it
-# for other needs.
+def checkModules():
+    missing = []
 
-for module in (
-    "sip", "PyQt4", "PyQt4.QtCore", "PyQt4.QtGui",
-    "PyKDE4", "PyKDE4.kdecore", "PyKDE4.kdeui", "PyKDE4.kparts", "PyKDE4.kio",
-    "PyKDE4.ktexteditor",
-    "dbus", "dbus.mainloop.qt",
-    ):
-    try:
-        exec "import %s" % module
-    except:
-        missing.append(module)
+    # these are checks for modules Frescobaldi needs, but you can tailor it
+    # for other needs.
 
-if missing:
-    sys.stderr.write("The following Python modules are missing:\n")
-    for m in missing:
-        sys.stderr.write("  %s\n" % m)
-    sys.exit(1)
+    for module in (
+        "sip", "PyQt4", "PyQt4.QtCore", "PyQt4.QtGui",
+        "PyKDE4", "PyKDE4.kdecore", "PyKDE4.kdeui", "PyKDE4.kparts", "PyKDE4.kio",
+        "PyKDE4.ktexteditor",
+        "dbus", "dbus.mainloop.qt",
+        ):
+        try:
+            exec ("import %s" % module) in globals()
+        except:
+            missing.append(module)
+    return missing
 
-errors = []
-mkver = lambda major, minor, release: major * 65536 + minor * 256 + release
+
+def checkVersions():
+    # versions
+    errors = []
     
-# versions
-if sys.version_info[:2] != (2, 6):
-    errors.append("Python version %s.%s.%s found, but need 2.6.\n"
-                  "(Use cmake -DPYTHON_EXECUTABLE=/path/to/python2.6)" %
-        sys.version_info[:3])
-        
-if sip.SIP_VERSION < mkver(4, 9, 1):
-    errors.append("(python-)sip version %s found, but need at least 4.9.1." %
-        sip.SIP_VERSION_STR)
-        
-if PyQt4.QtCore.PYQT_VERSION < mkver(4, 6, 0):
-    errors.append("PyQt4 version %s found, but need at least 4.6.0." %
-        PyQt4.QtCore.PYQT_VERSION_STR)
-        
-if PyKDE4.kdecore.pykde_version() < mkver(4, 0, 2):
-    errors.append("PyKDE4 version %s found, but need at least 4.0.2." %
-        PyKDE4.kdecore.pykde_versionString())
+    if sys.version_info[:2] != (2, 6):
+        errors.append("Python version %s.%s.%s found, but need 2.6.\n"
+                    "(Use cmake -DPYTHON_EXECUTABLE=/path/to/python2.6)" %
+            sys.version_info[:3])
+            
+    if sip.SIP_VERSION < mkver(4, 9, 1):
+        errors.append("(python-)sip version %s found, but need at least 4.9.1." %
+            sip.SIP_VERSION_STR)
+            
+    if PyQt4.QtCore.PYQT_VERSION < mkver(4, 6, 0):
+        errors.append("PyQt4 version %s found, but need at least 4.6.0." %
+            PyQt4.QtCore.PYQT_VERSION_STR)
+            
+    if PyKDE4.kdecore.pykde_version() < mkver(4, 0, 2):
+        errors.append("PyKDE4 version %s found, but need at least 4.0.2." %
+            PyKDE4.kdecore.pykde_versionString())
 
-if dbus.version < (0, 82, 4):
-    errors.append("python-dbus version %s found, but need at least 0.82.4." %
-        '.'.join(map(str, dbus.version)))
-        
-if errors:
-    sys.stderr.write("Some packages have outdated versions:\n")
-    for e in errors:
-        sys.stderr.write(e + '\n')
-    sys.exit(1)
+    if dbus.version < (0, 82, 4):
+        errors.append("python-dbus version %s found, but need at least 0.82.4." %
+            '.'.join(map(str, dbus.version)))
     
+    return errors
+
+
+if __name__ == "__main__":
+
+    missing = checkModules()
+    if missing:
+        sys.stderr.write("The following Python modules are missing:\n")
+        for m in missing:
+            sys.stderr.write("  %s\n" % m)
+        sys.exit(1)
+
+    errors = checkVersions()
+    if errors:
+        sys.stderr.write("Some packages have outdated versions:\n")
+        for e in errors:
+            sys.stderr.write(e + '\n')
+        sys.exit(1)
+        
