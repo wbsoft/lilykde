1f3999f52f28209ac2e341c1c0c004e1a9bf0d45
Wilbert Berendsen <wbsoft@xs4all.nl>
2010-08-26 10:54:36 +0000

    use builtin cmake commands
    git-svn-id: svn://localhost/tmp/repo1/trunk@2165 e61d349e-2442-45d7-aecb-3eed713e9aed

--- frescobaldi/cmake/scripts/uninstall_file.cmake
+++ frescobaldi/cmake/scripts/uninstall_file.cmake
@@ -1,12 +1,12 @@
 # Remove a file or symlink taking DESTDIR into account.
 # FILENAME should be defined on the command line.
+# If NO_CHECK is also defined, the file's existence is not checked
+# before removing. This can be used to remove a broken symlink.
 
 set(file "$ENV{DESTDIR}${FILENAME}")
 message(STATUS "Uninstalling: ${file}")
-execute_process(
-  COMMAND ${CMAKE_COMMAND} -E remove "${file}"
-  RESULT_VARIABLE rm_retval
-)
-if(rm_retval)
-  message(WARNING "Could not remove: ${file}")
-endif(rm_retval)
+if(DEFINED NO_CHECK OR EXISTS "${file}")
+  file(REMOVE "${file}")
+else(DEFINED NO_CHECK OR EXISTS "${file}")
+  message("File does not exist: ${file}")
+endif(DEFINED NO_CHECK OR EXISTS "${file}")
--- frescobaldi/cmake/scripts/uninstall_from_manifest.cmake
+++ frescobaldi/cmake/scripts/uninstall_from_manifest.cmake
@@ -6,20 +6,13 @@ if(NOT EXISTS "${MANIFEST}")
   message(FATAL_ERROR "Cannot find install manifest: \"${MANIFEST}\"")
 endif(NOT EXISTS "${MANIFEST}")
 
-file(READ "${MANIFEST}" files)
-string(REGEX REPLACE "\n" ";" files "${files}")
+file(STRINGS "${MANIFEST}" files)
 foreach(file ${files})
   set(file "$ENV{DESTDIR}${file}")
   message(STATUS "Uninstalling: ${file}")
   if(EXISTS "${file}")
-    execute_process(
-      COMMAND ${CMAKE_COMMAND} -E remove "${file}"
-      RESULT_VARIABLE rm_retval
-    )
-    if(rm_retval)
-      message(FATAL_ERROR "Problem when removing \"${file}\"")
-    endif(rm_retval)
+    file(REMOVE "${file}")
   else(EXISTS "${file}")
-    message(STATUS "File \"${file}\" does not exist.")
+    message("File does not exist: ${file}")
   endif(EXISTS "${file}")
 endforeach(file)
